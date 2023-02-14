85f5ccc226c93dca0468ec112e57f9cb299ba2ca
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-02-14 18:50:38 +0300

    added users entity

---
+++ src/main/java/com/derpate/bankapp/model/entity/UsersEntity.java
@@ -0,0 +1,49 @@
+package com.derpate.bankapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "patronymic_name")
+    private String patronymicName;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private String role;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "last_login")
+    private Timestamp lastLogin;
+}
