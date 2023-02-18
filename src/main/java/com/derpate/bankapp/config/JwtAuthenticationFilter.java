790f952a260a95d278a6fca1b9176a87158cf2db
AivazNurgaliev <aivaznurgaliev@gmail.com>
2023-02-18 15:11:58 +0300

    added jwt dependencies in pom.xml

--- src/main/java/com/derpate/bankapp/config/JwtAuthenticationFilter.java
+++ src/main/java/com/derpate/bankapp/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.derpate.bankapp.config;
 
+import com.derpate.bankapp.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -15,10 +16,22 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtService jwtService;
+    //Username as email
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
     }
 }
